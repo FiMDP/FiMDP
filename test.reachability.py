95accce3c5b1b2530fa5ec95f3813a2ecb4e99b4
Fanda Blahoudek <fandikb@gmail.com>
2019-10-23 14:38:39 -0500

    Make external files for examples, add test for reachability

--- test.reachability.py
+++ test.reachability.py
@@ -43,3 +43,36 @@ assert result == expected, ("get_positive_reachability() returns" +
 
 result = m.get_positiveReachability(set([2]), cap)
 expected = [31, 30, 0, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, inf, 32, 31, 32, 31, 32, 31, 32, 31, 32, 31, 32, 31]
+
+assert result == expected, ("get_positive_reachability() returns" +
+    " wrong values:\n" +
+    f"  expected: {expected}\n  returns:  {result}\n")
+
+from Kucera_example import m
+T = set([9])
+targets = [True if s in T else False for s in range(m.num_states)]
+m.add_action(4, {9:.5, 5:.5}, "t", 7)
+
+result = m.get_positiveReachability(T, 16)
+expected = [inf, 3, 2, 0, 0, 9, 14, 1, 1, 0, 5, inf, 1]
+
+assert result == expected, ("get_positive_reachability() returns" +
+    " wrong values:\n" +
+    f"  expected: {expected}\n  returns:  {result}\n")
+
+# Reduce the capacity so that we can't be sure to survive from 4.
+result = m.get_positiveReachability(T, 15)
+expected = [inf, inf, inf, inf, inf, inf, inf, inf, 1, 0, inf, inf, inf]
+
+assert result == expected, ("get_positive_reachability() returns" +
+    " wrong values:\n" +
+    f"  expected: {expected}\n  returns:  {result}\n")
+
+from reachability_example import m, targets
+
+result = m.get_positiveReachability(targets, 25)
+expected = [inf, inf, 2, 3, 3, 2, 1, 0, 7]
+
+assert result == expected, ("get_positive_reachability() returns" +
+    " wrong values:\n" +
+    f"  expected: {expected}\n  returns:  {result}\n")
