df617a93e112849feffcf99ddf6548cd6080276c
Fanda Blahoudek <fandikb@gmail.com>
2020-07-16 17:25:36 -0500

    Tutorial notebook that discusses the 3 solvers for Büchi objective.
    The notebook is `tut/StrategyTypes`. It is saved using jupytext as
    a .py file, but can be (and should be) opened using jupytext as a
    notebook.
    
    It is also used as test using the script tests/test.sh.

--- tests/test.sh
+++ tests/test.sh
@@ -5,4 +5,6 @@ python3 test_mecs.py
 python3 test_reachability.py
 python3 test_safety.py
 python3 test_strategy.py
+jupytext ../tutorials/StrategyTypes.py --to=ipynb
+jupyter nbconvert --execute ../tutorials/StrategyTypes.ipynb 
 
