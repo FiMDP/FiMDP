c7e843f0c5c2757a8b12193f62fcc97a7cf3245d
Fanda Blahoudek <fandikb@gmail.com>
2020-09-23 16:04:28 +0200

    Explicit encoding of energy to statespace -> `explicit.py`.
    `product_energy` and `get_MECs` are moved. The helper classes and
    functions are made private (hidden).

--- fimdp/explicit.py
+++ fimdp/explicit.py
@@ -1,17 +1,104 @@
+from collections import defaultdict
+
+from .core import ProductConsMDP
+
+
+def product_energy(cmdp, capacity, targets=[]):
+    """Explicit encoding of energy into state-space
+
+    The state-space of the newly created MDP consists of tuples `(s, e)`,
+    where `s` is the state of the input CMDP and `e` is the energy level.
+    For a tuple-state `(s,e)` and an action $a$ with consumption (in the
+    input CMDP) `c`, all successors of the action `a` in the new MDP are
+    of the form `(s', e-c)` for non-reload states and
+    `(r, capacity)` for reload states.
+    """
+    result = ProductConsMDP(cmdp, capacity)
+    # The list of output states for which we have not yet
+    # computed the successors.  Items on this list are triplets
+    # of the form `(s, e, p)` where `s` is the state
+    # number in the mdp, `e` is the energy level, and p
+    # is the state number in the output mdp.
+    todo = []
+    otargets = []
+    sink_created = False
+
+    # Transform a pair of state numbers (s, e) into a state
+    # number in the output mdp, creating a new state if needed.
+    # Whenever a new state is created, we can add it to todo.
+    def dst(s, e):
+        p = result.get_state(s, e)
+        if p is None:
+            p = result.new_state(s, e,
+                                 reload=cmdp.is_reload(s))
+            if s in targets and e >= 0:
+                otargets.append(p)
+            todo.append((s, e, p))
+        return p
+
+    # Initialization
+    # For each state of mdp add a new initial state
+    for s in range(cmdp.num_states):
+        dst(s, capacity)
+
+    # Build all states and edges in the product
+    while todo:
+        s, e, p = todo.pop()
+        for a in cmdp.actions_for_state(s):
+            # negative goes to sink
+            if e - a.cons < 0:
+                if not sink_created:
+                    sink = result.new_state(-1, "-∞", name="sink,-∞")
+                    result.add_action(sink, {sink: 1}, "σ", 1, None)
+                    sink_created = True
+                result.add_action(p, {sink: 1}, a.label, a.cons, a)
+                continue
+            # build new distribution
+            odist = {}
+            for succ, prob in a.distr.items():
+                new_e = capacity if cmdp.is_reload(succ) else e - a.cons
+                out_succ = dst(succ, new_e)
+                odist[out_succ] = prob
+            result.add_action(p, odist, a.label, a.cons, a)
+
+    return result, otargets
+
+
 # Decompose MDP into MECs. Ignores consumption.
 #
 # The algorithm uses decomposition of directed graph using
 # Tarjan's algorithm (single DFS). The implementation of this
 # the Tarjan's algo is inspired from:
 # https://www.geeksforgeeks.org/tarjan-algorithm-find-strongly-connected-components/
+def get_MECs(mdp):
+    """Given an MDP (not necessarly consMDP), compute its
+    maximal-end-components decomposition.
 
-from collections import defaultdict
+    Returns list of mecs (lists).
+    """
+    g = _mdp2graph(mdp)
+    mecs = []
+    removed = set()
+    # detect states of bSCCs
+    while len(g.graph) > 0:
+        to_remove = set()
+        sccs = g.SCC()
+        for scc_i, scc in enumerate(sccs):
+            if g.check_bscc(scc) and not g.check_trivial(scc):
+                to_remove.update(scc)
+                mecs.append(scc)
+
+        attr = _prob_attractor(mdp, removed.union(to_remove))
+        g.remove_vertices(attr)
+        removed.update(attr)
+
+    return mecs
 
 
-# This class represents an directed graph
-# using adjacency list representation
-class Graph:
-
+class _Graph:
+    '''
+    Represent graphs using adjacency list representation
+    '''
     def __init__(self):
         # default dictionary to store graph
         self.graph = defaultdict(list)
@@ -123,7 +210,7 @@ class Graph:
         return True
 
 
-def prob_attractor(mdp, attr):
+def _prob_attractor(mdp, attr):
     """ Compute a set of states from which I cannot avoid
     states from SCCs pointed by `scc_ind`.
     scc_indices are pointers to graph_sccs.
@@ -150,38 +237,13 @@ def prob_attractor(mdp, attr):
     return attr
 
 
-def mdp2graph(mdp):
+def _mdp2graph(mdp):
     """Convert mdp to graph representation for the Tarjan algo"""
     ns = mdp.num_states # store the number of states
-    g = Graph()
+    g = _Graph()
 
     for s in range(ns):
         for t in mdp.state_succs(s):
             g.addEdge(s,t)
 
     return g
-
-
-def get_MECs(mdp):
-    """Given an MDP (not necessarly consMDP), compute its
-    maximal-end-components decomposition.
-
-    Returns list of mecs (lists).
-    """
-    g = mdp2graph(mdp)
-    mecs = []
-    removed = set()
-    # detect states of bSCCs
-    while len(g.graph) > 0:
-        to_remove = set()
-        sccs = g.SCC()
-        for scc_i, scc in enumerate(sccs):
-            if g.check_bscc(scc) and not g.check_trivial(scc):
-                to_remove.update(scc)
-                mecs.append(scc)
-
-        attr = prob_attractor(mdp, removed.union(to_remove))
-        g.remove_vertices(attr)
-        removed.update(attr)
-
-    return mecs
