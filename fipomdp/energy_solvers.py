fa93083808600a02ea652b3908709819c3a961b6
brlejs <simon.brlej@mavenir.com>
2021-12-10 12:41:51 +0100

    feat-002-Adding buchi safe action filter on belief supp cmdp

--- fipomdp/energy_solvers.py
+++ fipomdp/energy_solvers.py
@@ -5,10 +5,10 @@ Supported objectives:
  * positive reachability(target)
  * büchi(target)
 """
+import math
+from typing import List, Dict, Optional, Tuple
 
-from typing import List, Dict, Optional
-
-from fimdp.core import Strategy, ConsMDP, CounterSelector
+from fimdp.core import Strategy, ConsMDP, ActionData
 from fimdp.energy_solvers import BasicES
 from fimdp.objectives import SAFE, POS_REACH, BUCHI
 from fipomdp import ConsPOMDP
@@ -21,17 +21,15 @@ class ConsPOMDPBasicES:
     guessing_ES: BasicES
     cap: int
 
-    min_levels: Dict[int, List[int]]
-    helper_levels: Dict[int, List[int]]
-
-    guess_selector: CounterSelector
+    bs_min_levels: Dict[int, List[int]]
+    guess_min_levels: Dict[int, List[int]]
 
     def __init__(
-        self,
-        cpomdp: ConsPOMDP,
-        belief_supp: List[int],
-        cap: int,
-        cpomdp_targets: List[int],
+            self,
+            cpomdp: ConsPOMDP,
+            belief_supp: List[int],
+            cap: int,
+            cpomdp_targets: List[int],
     ):
         self.cpomdp = cpomdp
         self.cap = cap
@@ -41,19 +39,19 @@ class ConsPOMDPBasicES:
 
         guessing_cmdp_targets = []
         for i in range(
-            self.cpomdp.guessing_cmdp.num_states
+                self.cpomdp.guessing_cmdp.num_states
         ):  # convert pomdp targets into guess targets
             if (
-                self.cpomdp.guessing_cmdp.belief_supp_guess_pairs[i][1]
-                in cpomdp_targets
+                    self.cpomdp.guessing_cmdp.belief_supp_guess_pairs[i][1]
+                    in cpomdp_targets
             ):
                 guessing_cmdp_targets.append(i)
         self.guessing_ES = BasicES(
             self.cpomdp.guessing_cmdp, cap, guessing_cmdp_targets
         )
 
-        self.min_levels = {}
-        self.helper_levels = {}
+        self.bs_min_levels = {}
+        self.guess_min_levels = {}
 
     def _guessing_min_safe_levels(self, bel_supp_min_levels: List[int]) -> List[int]:
         min_levels = []
@@ -69,13 +67,13 @@ class ConsPOMDPBasicES:
 
     def compute_safe(self) -> None:
         self.bel_supp_ES.compute(SAFE)
-        self.min_levels[SAFE] = self._guessing_min_safe_levels(
+        self.bs_min_levels[SAFE] = self._guessing_min_safe_levels(
             self.bel_supp_ES.min_levels[SAFE]
         )
 
     def compute_posreach(self) -> None:
         self.guessing_ES.compute(POS_REACH)
-        self.min_levels[POS_REACH] = self.guessing_ES.min_levels[POS_REACH]
+        self.guess_min_levels[POS_REACH] = self.guessing_ES.min_levels[POS_REACH]
 
     def compute_buchi(self) -> None:
         original_reloads = list(self.cpomdp.guessing_cmdp.reloads)
@@ -85,8 +83,8 @@ class ConsPOMDPBasicES:
             self.compute_posreach()
             for i in range(self.cpomdp.guessing_cmdp.num_states):
                 if (
-                    self.cpomdp.guessing_cmdp.is_reload(i)
-                    and self.min_levels[POS_REACH][i] > self.cap
+                        self.cpomdp.guessing_cmdp.is_reload(i)
+                        and self.guess_min_levels[POS_REACH][i] > self.cap
                 ):
                     fixpoint = False
                     unusable_reloads = self.cpomdp.guessing_cmdp.belief_supp_states(
@@ -94,20 +92,27 @@ class ConsPOMDPBasicES:
                     )
                     for state in unusable_reloads:
                         self.cpomdp.guessing_cmdp.set_reload(state, False)
-        self.min_levels[BUCHI] = list(self.min_levels[POS_REACH])
+
+        self.guess_min_levels[BUCHI] = list(self.guess_min_levels[POS_REACH])
+        bs_min_levels = [-1 for _ in range(self.cpomdp.belief_supp_cmdp.num_states)]
+
+        for i in range(self.cpomdp.guessing_cmdp.num_states):
+            belief_support = self.cpomdp.guessing_cmdp.belief_supp_guess_pairs[i][0]
+            bs_min_levels[self.cpomdp.belief_supp_cmdp.bel_supp_indexer[tuple(belief_support)]] = self.guess_min_levels[i]
+
+        self.bs_min_levels[BUCHI] = bs_min_levels
         self.cpomdp.guessing_cmdp.reloads = original_reloads
         self.compute_posreach()
-        self.guess_selector = self.guessing_ES.get_selector(BUCHI)
 
+    def get_buchi_safe_actions_bscmdp(self) -> List[Tuple[int, ActionData]]:
+        bsafe_actions = []
+        for i in range(self.cpomdp.belief_supp_cmdp.num_states):
+            for action in self.cpomdp.belief_supp_cmdp.actions_for_state(i):
+                safe_level = action.cons
+                for succ in action.get_succs():
+                    if not self.cpomdp.belief_supp_cmdp.reloads[succ]:
+                        safe_level = max(safe_level, action.cons + self.bs_min_levels[succ])
+                bsafe_actions.append((safe_level, action))
 
-class POMDPStrategy(Strategy):
-    def _next_action(self):
-        pass
-
-    cap: int
-
-    def __init__(
-        self, mdp: ConsMDP, cap: int, init_state: Optional[int] = None, *args, **kwargs
-    ):
-        super(POMDPStrategy, self).__init__(mdp, init_state, args, kwargs)
-        self.cap = cap
+        return bsafe_actions
+            
