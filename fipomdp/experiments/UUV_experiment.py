753dfe2fa5e8c433a0135347b50c1b30ecd09fb1
brlejs <simon.brlej@mavenir.com>
2022-01-14 13:59:54 +0100

    feat-002-Modifying-experiment

---
+++ fipomdp/experiments/UUV_experiment.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+import logging
+import multiprocessing
+import platform, psutil
+from typing import List, Tuple, Dict
+from joblib import Parallel, delayed
+
+from fimdp.core import ActionData
+from fimdp.objectives import BUCHI
+from fimdpenv.UUVEnv import SingleAgentEnv
+from fipomdp import ConsPOMDP, log_utils
+from fipomdp.energy_solvers import ConsPOMDPBasicES
+from fipomdp.pomcp import OnlineStrategy
+from fipomdp.environment_utils import set_cross_observations_to_UUV_grid
+from fipomdp.pomcp_utils import matching_state_action, sample_from_distr
+
+
+def uuv_experiment(computed_cpomdp: ConsPOMDP, computed_solver: ConsPOMDPBasicES, capacity: int, targets: List[int], random_seed: int, logger) -> \
+Tuple[Dict[int, int], List[int]]:
+
+    logger = logger
+
+    if computed_cpomdp.belief_supp_cmdp is None or computed_solver.bs_min_levels[BUCHI] is None:
+        raise AttributeError(f"Given CPOMDP or its solver is not pre computed!")
+
+    init_energy = capacity
+    init_obs = 399
+    init_bel_supp = tuple([399])
+    exploration = 1
+    rollout_horizon = 100
+
+    strategy = OnlineStrategy(
+        computed_cpomdp,
+        capacity,
+        init_energy,
+        init_obs,
+        init_bel_supp,
+        targets,
+        exploration,
+        rollout_horizon=rollout_horizon,
+        random_seed=random_seed,
+        recompute=False,
+        solver=computed_solver,
+        logger=logger
+    )
+
+    simulated_state = init_bel_supp[0]
+
+    path = [simulated_state]
+
+    max_iteration_target_reached_count = {1000: 0}
+
+    for max_iterations in max_iteration_target_reached_count.keys():  # max iterations
+
+        logger.info(f"\nLAUNCHING with max iterations: {max_iterations}\n")
+
+        for j in range(100):  # 100 actions
+            action = strategy.next_action(max_iterations)
+            simulated_state, new_obs = simulate_observation(computed_cpomdp, action, simulated_state)
+            path.append(simulated_state)
+            if simulated_state in targets:
+                max_iteration_target_reached_count[max_iterations] += 1
+            strategy.update_obs(new_obs)
+
+    logger.info(f"\n--------EXPERIMENT FINISHED---------")
+    logger.info(f"--------RESULTS--------")
+    for k, v in max_iteration_target_reached_count.items():
+        logger.info(f"For max iterations: {k}, target has been reached {v} times.")
+        logger.info(f"Path of the agent was: {path}")
+
+    return max_iteration_target_reached_count, path
+
+
+def simulate_observation(cpomdp: ConsPOMDP, bs_action: ActionData, src_state: int) -> Tuple[int, int]:
+    cpomdp_action = matching_state_action(cpomdp, bs_action, src_state)
+    new_state = sample_from_distr(cpomdp_action.distr)
+    new_obs = sample_from_distr(cpomdp.get_state_obs_probs(new_state))
+    return new_state, new_obs
+
+
+def log_experiment_with_seed(cpomdp, env, i, log_file_name, solver, targets):
+    handler = logging.FileHandler(f"./logs/{log_file_name}{i}.log", 'w')
+    formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(f"{i}")
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+    logger.addHandler(handler)
+    logger.level = logging.INFO
+    logger.info("START")
+    uname = platform.uname()
+    logger.info(f"Node name: {uname.node}")
+    logger.info(f"System: {uname.system}")
+    logger.info(f"Release: {uname.release}")
+    logger.info(f"Version: {uname.version}")
+    logger.info(f"Machine: {uname.machine}")
+    logger.info(f"Processor: {uname.processor}")
+    logger.info(f"RAM: {str(round(psutil.virtual_memory().total / (1024.0 ** 3)))} GB")
+    return uuv_experiment(cpomdp, solver, env.capacities[0], targets, i, logger)
+
+
+def main():
+    log_file_name = "UUVExperimentsFromPythonFile"  # Change for your needs
+    logging_level = logging.INFO
+    # set to INFO (20) for logging to be active, set to DEBUG (10) for details,
+    # set to 5 for extreme debug
+
+    logging.basicConfig(
+        filename=f"{log_file_name}.log",
+        filemode="w",  # Erase previous log
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging_level,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    env = SingleAgentEnv(
+        grid_size=[20, 20],
+        capacity=20,
+        reloads=[i*20+j for i in range(0, 20, 3) for j in range(0, 20, 3)],  # reloads in 3x3 grids
+        targets=[103, 209, 210, 270],
+        init_state=399,
+        enhanced_actionspace=0,
+    )
+
+    mdp, targets = env.get_consmdp()
+    mdp.__class__ = ConsPOMDP
+    set_cross_observations_to_UUV_grid(mdp, (env.grid_size[0], env.grid_size[1]))
+
+    cpomdp = mdp
+    cpomdp.compute_guessing_cmdp_initial_state([399])
+
+    solver = ConsPOMDPBasicES(cpomdp, [399], env.capacities[0], targets)
+    solver.compute_buchi()
+
+    results = Parallel(n_jobs=10)(delayed(log_experiment_with_seed)(cpomdp, env, i, log_file_name, solver, targets) for i in range(10))
+
+    logging.info(f"RESULTS (): {results}")
+
+if __name__ == "__main__":
+    main()
