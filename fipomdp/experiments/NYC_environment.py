3bb616d11ec38d5dc5bb237997afd0fae2c334ee
brlejs <simon.brlej@mavenir.com>
2022-02-16 14:39:06 +0100

    feat-002-nyc-experiment

---
+++ fipomdp/experiments/NYC_environment.py
@@ -0,0 +1,155 @@
+import re
+from copy import copy
+from typing import Tuple, List
+
+from fimdp.core import ActionData, ConsMDP
+from fimdpenv.AEVEnv import AEVEnv
+from fipomdp import ConsPOMDP
+
+
+class NYCPOMDPEnvironment:
+
+    cmdp_env: AEVEnv
+
+    def __init__(self):
+        reloads = ['42431659', '42430367', '1061531810', '42443056', '1061531448', '42448735', '596775930', '42435275',
+                   '42429690', '42446036', '42442528', '42440966', '42431186', '42438503', '42442977',
+                   '42440966', '1061531802', '42455666']
+        targets = ['42440465', '42445916']
+
+        self.cmdp_env = AEVEnv(capacity=1000, targets=targets, reloads=reloads, init_state='42459137')
+        self.cmdp_env.reset(init_state='42459137', init_energy=1000)
+
+    def get_cpomdp(self) -> Tuple[ConsPOMDP, List[int]]:
+
+        mdp, targets = self.cmdp_env.get_consmdp()
+
+        original_num_states = mdp.num_states
+
+        # States
+
+        for state in range(original_num_states):
+            mdp.new_state(mdp.reloads[state], mdp.names[state]+"_low")
+
+        for state in range(original_num_states):
+            mdp.new_state(mdp.reloads[state], mdp.names[state]+"_high")
+
+        print([mdp.names[state] for state in range(mdp.num_states)])
+
+        for state in range(original_num_states):
+            actions = mdp.actions_for_state(state)
+            for action in actions:
+                orig_distr = {k: float(v) for k, v in action.distr.items()}
+                action.distr = orig_distr  # AEVEnv uses decimal not float values, fix
+                for i in range(1, 3):
+                    index_shift = i*original_num_states
+                    src = action.src+index_shift
+                    shifted_distr = {k+index_shift: v for k, v in orig_distr.items()}
+                    mdp.add_action(src, shifted_distr, action.label, action.cons)
+
+        for i in range(10):
+            for action in mdp.actions_for_state(i):
+                print(action)
+            for action in mdp.actions_for_state(i+original_num_states):
+                print(action)
+
+        for state in range(original_num_states):
+            if re.search("^\d+$", mdp.names[state]):  # regex matching exactly number names
+                actions = mdp.actions_for_state(state)  # mid, low, high traffic state changes - actions
+                for act in actions:
+                    self._modify_stochastic_action(mdp, act, original_num_states)
+
+            if re.search("^ps\d+_\d+_act\d+$", mdp.names[state]):  # regex matching exactly number + '_act' + number
+                actions = mdp.actions_for_state(state)
+                for act in actions:
+                    self._modify_dummy_action(mdp, act, original_num_states)
+
+        state_obs_probs = {}
+
+        for obs in range(original_num_states):
+            state_obs_probs[(obs, obs)] = 1  # mid state
+            state_obs_probs[(obs+original_num_states, obs)] = 1  # low state
+            state_obs_probs[(obs+2*original_num_states, obs)] = 1  # high state
+
+        mdp.__class__ = ConsPOMDP
+
+        observations = {}
+
+        for i in range(3):
+            for state in range(original_num_states):
+                observations[(state + i*original_num_states, state)] = 1
+
+        mdp.set_observations(original_num_states, observations)
+
+        return mdp, targets
+
+    def _modify_dummy_action(self, mdp: ConsMDP, mid_action: ActionData, shift: int):
+        orig_distr = mid_action.distr
+
+        if len(orig_distr.keys()) != 1:
+            print(mdp.names[mid_action.src])
+            print([(mdp.names[dest_state], val) for dest_state, val in mid_action.distr.items()])
+            raise AttributeError("Given action is not one to one.")
+
+        dest = list(orig_distr.keys())[0]
+        new_distr = {dest: 0.5, dest+shift: 0.25, dest+2*shift: 0.25}
+        mid_action.distr = new_distr
+
+        low_src = mid_action.src + shift
+        high_src = mid_action.src + 2*shift
+        low_label = mid_action.label  # + "_low"
+        high_label = mid_action.label  # + "_high"
+
+        low_action = [act for act in mdp.actions_for_state(low_src) if act.label == low_label][0]
+        high_action = [act for act in mdp.actions_for_state(high_src) if act.label == high_label][0]
+
+        low_action_distr = {dest: 0.75, dest+shift: 0.25}
+        high_action_distr = {dest: 0.75, dest+2*shift: 0.25}
+
+        low_action.distr = low_action_distr
+        high_action.distr = high_action_distr
+
+    def _modify_stochastic_action(self, mdp: ConsMDP, mid_action: ActionData, shift: int):
+        orig_distr = mid_action.distr
+
+        action_number = mid_action.label[-1]
+
+        if len(orig_distr.keys()) != 3:
+            raise AttributeError("Given action is not one to three, three stochastic cost states required.")
+
+        dest_states = list(orig_distr.keys())
+        dest_names = [mdp.names[s] for s in dest_states]
+
+        if not all(map(lambda name: re.search('^ps\d_\d+_act\d+$', name) is not None, dest_names)):
+            raise AttributeError(f"Incorrect destination states, expected names in starting with ps: 'ps.....' got, {dest_names}.")
+
+        low_src = mid_action.src + shift
+        high_src = mid_action.src + 2*shift
+        low_label = mid_action.label  # + "_low"
+        high_label = mid_action.label  # + "_high"
+
+        low_action = [act for act in mdp.actions_for_state(low_src) if act.label == low_label][0]
+        high_action = [act for act in mdp.actions_for_state(high_src) if act.label == high_label][0]
+
+        ps1_state_index = dest_names.index([name for name in dest_names if re.search(f'^ps1_\d+_act{action_number}$', name)][0])
+        ps3_state_index = dest_names.index([name for name in dest_names if re.search(f'^ps3_\d+_act{action_number}$', name)][0])
+
+        ps1_state = dest_states[ps1_state_index]
+        ps3_state = dest_states[ps3_state_index]
+
+        print(ps3_state_index)
+        print(ps1_state_index)
+
+        low_dist = copy(low_action.distr)
+        low_dist[ps3_state + shift] += low_dist[ps1_state + shift]
+        low_dist.pop(ps1_state + shift)
+
+        high_dist = copy(high_action.distr)
+        high_dist[ps1_state + 2*shift] += high_dist[ps3_state + 2*shift]
+        high_dist.pop(ps3_state + 2*shift)
+
+        if len(low_dist) != 2 or len(high_dist) != 2:
+            raise IndexError(f"Incorrect indexing.")
+
+        low_action.distr = low_dist
+        high_action.distr = high_dist
---
+++ fipomdp/experiments/NYC_experiment.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+import logging
+import platform
+from typing import List, Tuple, Dict
+
+import psutil
+from joblib import Parallel, delayed
+
+from fimdp.objectives import BUCHI
+from fipomdp import ConsPOMDP
+from fipomdp.energy_solvers import ConsPOMDPBasicES
+from fipomdp.experiments.NYC_environment import NYCPOMDPEnvironment
+from fipomdp.experiments.UUV_experiment import simulate_observation
+from fipomdp.pomcp import OnlineStrategy
+
+
+def nyc_experiment(computed_cpomdp: ConsPOMDP, computed_solver: ConsPOMDPBasicES, capacity: int, targets: List[int], random_seed: int, logger) -> \
+Tuple[Dict[int, int], List[int]]:
+    logger = logger
+
+    if computed_cpomdp.belief_supp_cmdp is None or computed_solver.bs_min_levels[BUCHI] is None:
+        raise AttributeError(f"Given CPOMDP or its solver is not pre computed!")
+
+    init_energy = capacity
+    init_obs = 399
+    init_bel_supp = tuple([399])
+    exploration = 1
+    rollout_horizon = 100
+
+    strategy = OnlineStrategy(
+        computed_cpomdp,
+        capacity,
+        init_energy,
+        init_obs,
+        init_bel_supp,
+        targets,
+        exploration,
+        rollout_horizon=rollout_horizon,
+        random_seed=random_seed,
+        recompute=False,
+        solver=computed_solver,
+        logger=logger
+    )
+
+    simulated_state = init_bel_supp[0]
+
+    path = [simulated_state]
+
+    max_iteration_target_reached_count = {1000: 0}
+
+    for max_iterations in max_iteration_target_reached_count.keys():  # max iterations
+
+        logger.info(f"\nLAUNCHING with max iterations: {max_iterations}\n")
+
+        for j in range(100):  # 100 actions
+            action = strategy.next_action(max_iterations)
+            simulated_state, new_obs = simulate_observation(computed_cpomdp, action, simulated_state)
+            path.append(simulated_state)
+            if simulated_state in targets:
+                max_iteration_target_reached_count[max_iterations] += 1
+            strategy.update_obs(new_obs)
+
+    logger.info(f"\n--------EXPERIMENT FINISHED---------")
+    logger.info(f"--------RESULTS--------")
+    for k, v in max_iteration_target_reached_count.items():
+        logger.info(f"For max iterations: {k}, target has been reached {v} times.")
+        logger.info(f"Path of the agent was: {path}")
+
+    return max_iteration_target_reached_count, path
+
+
+def log_experiment_with_seed(cpomdp, env, i, log_file_name, solver, targets):
+    handler = logging.FileHandler(f"./logs/{log_file_name}{i}.log", 'w')
+    formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(f"{i}")
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+    logger.addHandler(handler)
+    logger.level = logging.INFO
+    logger.info("START")
+    uname = platform.uname()
+    logger.info(f"Node name: {uname.node}")
+    logger.info(f"System: {uname.system}")
+    logger.info(f"Release: {uname.release}")
+    logger.info(f"Version: {uname.version}")
+    logger.info(f"Machine: {uname.machine}")
+    logger.info(f"Processor: {uname.processor}")
+    logger.info(f"RAM: {str(round(psutil.virtual_memory().total / (1024.0 ** 3)))} GB")
+    return nyc_experiment(cpomdp, solver, env.capacities[0], targets, i, logger)
+
+
+def main():
+    log_file_name = "NYCExperiments"  # Change for your needs
+    logging_level = logging.INFO
+    # set to INFO (20) for logging to be active, set to DEBUG (10) for details,
+    # set to 5 for extreme debug
+
+    logging.basicConfig(
+        filename=f"{log_file_name}.log",
+        filemode="w",  # Erase previous log
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging_level,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    env = NYCPOMDPEnvironment()
+    cpomdp, targets = env.get_cpomdp()
+
+    cpomdp.compute_guessing_cmdp_initial_state([cpomdp.state_with_name('42434894')])
+
+    solver = ConsPOMDPBasicES(cpomdp, [cpomdp.state_with_name('42434894')], env.cmdp_env.capacity, targets)
+    solver.compute_buchi()
+
+    results = Parallel(n_jobs=10)(
+        delayed(log_experiment_with_seed)(cpomdp, env, i, log_file_name, solver, targets) for i in range(10))
+
+    logging.info(f"RESULTS (): {results}")
+
+
+if __name__ == "__main__":
+    main()
+
