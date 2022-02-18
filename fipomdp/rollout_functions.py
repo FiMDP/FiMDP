2f75259463e132b1d6dd5f0fa1880f4bd2bfde9e
brlejs <simon.brlej@mavenir.com>
2022-02-18 10:19:07 +0100

    feat-002-Adding rollout functions

---
+++ fipomdp/rollout_functions.py
@@ -0,0 +1,31 @@
+from typing import Tuple, List
+
+
+def basic(state: int, steps: int, consumed_energy: int, reload_count: int, remaining_energy: int,
+          target_found: bool) -> float:
+    return 1 if target_found else 0.1
+
+
+def step_based(state: int, steps: int, consumed_energy: int, reload_count: int, remaining_energy: int,
+               target_found: bool) -> float:
+    return -steps if target_found else -2 * steps
+
+
+def consumption_based(state: int, steps: int, consumed_energy: int, reload_count: int, remaining_energy: int,
+                      target_found: bool) -> float:
+    return -consumed_energy + remaining_energy
+
+
+def grid_manhattan_distance(state: int, steps: int, consumed_energy: int, reload_count: int, remaining_energy: int,
+                            target_found: bool, grid_size: Tuple[int, int], targets: List[int]) -> float:
+    if target_found:
+        return -steps
+    else:
+        x_dists = [abs(state / grid_size[0] - target / grid_size[0]) for target in targets]
+        y_dists = [abs(state % grid_size[0] - target % grid_size[0]) for target in targets]
+
+        return -steps - min(x_dists[i] + y_dists[i] for i in range(len(x_dists)))
+
+def product(state: int, steps: int, consumed_energy: int, reload_count: int, remaining_energy: int, target_found:bool,
+            a: int, b: int) -> float:
+    return a * b
